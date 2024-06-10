be8866edb327d40fd85a093899cf8e19458f8b66
Yoo013 <alfraka744@gmail.com>
2024-06-10 17:25:28 +0700

    upsdda

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,6 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import Signup from "./components/Singup";
-import Login from "./components/Login";
 
 import App2 from "./App2";
 import App3 from "./App3";
@@ -17,8 +15,6 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<App3/>} />
-        <Route path="/signup" element={<Signup/>} />
-        <Route path="/login" element={<Login/>} />
         <Route path="/home" element={<App2/>} />
       
       </Routes>
